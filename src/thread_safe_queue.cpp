53aedbf57b5e2db8cc5f4b47a8ce3c0ee1288b06
Abdallah Khater <abdallah.khater.eng@gmail.com>
2026-06-26 12:09:55 +0300

    fix(queue): optimize lock scope and fix missing notify
    - Release lock prior to notify_one to reduce thread wake-up latency.
    - Add missing m_cv_not_full.notify_one() inside the pop method to prevent permanent producer thread starvation when the queue is full.

--- src/thread_safe_queue.cpp
+++ src/thread_safe_queue.cpp
@@ -4,22 +4,28 @@ ThreadSafeQueue::ThreadSafeQueue(size_t max_capacity) : m_max_capacity{max_capac
 
 void ThreadSafeQueue::push(std::string log_line)
 {
-    std::unique_lock lk{m_queue_mutex};
+    {
+        std::unique_lock lk{m_queue_mutex};
 
-    m_cv_not_full.wait(lk, [this] { return m_data_queue.size() < m_max_capacity; });
+        m_cv_not_full.wait(lk, [this] { return m_data_queue.size() < m_max_capacity; });
 
-    m_data_queue.push(std::move(log_line));
+        m_data_queue.push(std::move(log_line));
+    }
 
     m_cv_not_empty.notify_one();
 }
 
 void ThreadSafeQueue::pop(std::string& value)
 {
-    std::unique_lock lk{m_queue_mutex};
+    {
+        std::unique_lock lk{m_queue_mutex};
 
-    m_cv_not_empty.wait(lk, [this] { return !m_data_queue.empty(); });
+        m_cv_not_empty.wait(lk, [this] { return !m_data_queue.empty(); });
 
-    value = m_data_queue.front();
+        value = m_data_queue.front();
 
-    m_data_queue.pop();
+        m_data_queue.pop();
+    }
+
+    m_cv_not_full.notify_one();
 }
