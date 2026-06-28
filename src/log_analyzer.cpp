a440b3425d87705ae40218d09c1b092618391414
Abdallah Khater <abdallah.khater.eng@gmail.com>
2026-06-28 20:16:05 +0300

    feat(pipeline): add graceful shutdown via stop tokens
    - Updated LogReader, LogAnalyzer, and LogWriter functors to accept std::stop_token.

--- src/log_analyzer.cpp
+++ src/log_analyzer.cpp
@@ -17,7 +17,8 @@ bool check_for_issue(const std::string& line)
 
 } // Anonymous namespace
 
-LogAnalyzer::LogAnalyzer(ThreadSafeQueue<std::string>& src_queue, ThreadSafeQueue<std::string>& dest_queue)
+LogAnalyzer::LogAnalyzer(ThreadSafeQueue<std::string>& src_queue,
+                         ThreadSafeQueue<std::string>& dest_queue)
     : m_src_queue{src_queue}, m_dest_queue{dest_queue}
 {
 }
@@ -26,13 +27,16 @@ void LogAnalyzer::operator()(std::stop_token st)
 {
 
     std::string line;
-    while (!st.stop_requested())
+    while (true)
     {
-        m_src_queue.pop(line);
+        if (!m_src_queue.pop(line, st)) { break; }
 
-        if (check_for_issue(line)) { m_dest_queue.push(line); }
+        if (check_for_issue(line))
+        {
+            if (!m_dest_queue.push(line, st)) { break; }
+        }
     }
 
     // Graceful shutdown sequence when stop token is triggered
-    std::cout << "[LogAnalyzer] Shutting down.\n";
+    std::cerr << "[LogAnalyzer] Stop token received. shutting down.\n";;
 }
